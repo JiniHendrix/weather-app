cadeb0855f4c6355c713c8e9a01b211283514d74
Jin Choi <jinchoi@ucdavis.edu>
2020-05-04 16:07:42 -0700

    added showing of lat/lon of los angeles

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const request = require('postman-request');
 
 const url = 'http://api.weatherstack.com/current?access_key=fd6621ff82539cd3e25a24d3d6243596&query=Los%20Angeles';
 
-request(url, (err, res, body) => {
-  const parsedBody = JSON.parse(body);
+request({ url, json: true }, (err, res, body) => {
+  console.log(`It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees`);
+});
 
-  console.log(`It is currently ${parsedBody.current.temperature} degrees out. It feels like ${parsedBody.current.feelslike} degrees`);
+const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiamluaWhlbmRyaXgiLCJhIjoiY2s5dDJlYzFjMDA4MDNucG43c3dxM2ZhcyJ9.5gUfNd3yNgXZWtsu8owG8A';
+
+request({ url: geoUrl, json: true }, (err, res, body) => {
+  console.log(body.features[0].center)
 });
