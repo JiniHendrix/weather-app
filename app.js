789e98bf2f7c9c72de5517535aa35e963ec19270
Jin Choi <jinchoi@ucdavis.edu>
2020-05-04 15:25:49 -0700

    added showing of temperature/feelslike temperature

--- app.js
+++ app.js
@@ -3,5 +3,7 @@ const request = require('postman-request');
 const url = 'http://api.weatherstack.com/current?access_key=fd6621ff82539cd3e25a24d3d6243596&query=Los%20Angeles';
 
 request(url, (err, res, body) => {
-  console.log(body)
+  const parsedBody = JSON.parse(body);
+
+  console.log(`It is currently ${parsedBody.current.temperature} degrees out. It feels like ${parsedBody.current.feelslike} degrees`);
 });
