8b4ff957f3427e0298c7d7d47c2508f8999b4cf8
Jin Choi <jinchoi@ucdavis.edu>
2020-05-04 16:26:29 -0700

    Added error handling

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const request = require('postman-request');
 
-const url = 'http://api.weatherstack.com/current?access_key=fd6621ff82539cd3e25a24d3d6243596&query=Los%20Angeles';
+const url = 'http://api.weatherstack.com/current?access_key=fd6621ff82539cd3e25a24d3d6243596&query=Los Angeles';
 
 request({ url, json: true }, (err, res, body) => {
-  console.log(`It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees`);
+  if (err) {
+    console.log('Unable to connect to weather service!')
+  } else if (body.error) {
+    console.log('Unable to find location');
+  } else {
+    console.log(`It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees`);
+  }
 });
 
-const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiamluaWhlbmRyaXgiLCJhIjoiY2s5dDJlYzFjMDA4MDNucG43c3dxM2ZhcyJ9.5gUfNd3yNgXZWtsu8owG8A';
+const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los Angeles.json?access_token=pk.eyJ1IjoiamluaWhlbmRyaXgiLCJhIjoiY2s5dDJlYzFjMDA4MDNucG43c3dxM2ZhcyJ9.5gUfNd3yNgXZWtsu8owG8A';
 
 request({ url: geoUrl, json: true }, (err, res, body) => {
-  console.log(body.features[0].center)
+  if (err) {
+    console.log('Unable to connect to geo service!')
+  } else if (!body.features.length) {
+    console.log('Location not found!');
+  } else {
+    console.log(...body.features[0].center)
+  }
 });
