3d0d39f1c4a8ef99bebccfe53bdad5546d135294
Jin Choi <jinchoi@ucdavis.edu>
2020-05-06 10:51:47 -0700

    made location be taken from CLI. made temp units be fahrenheit

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const { geocode, forecast } = require('./utils');
 
-geocode('New York', (error, data) => {
+geocode(process.argv[2], (error, data) => {
   if (error) {
     return console.error('Error: ', error);
   }
-
-  console.log('Data: ', data);
 
   forecast(data.latitude, data.longitude, (error, data) => {
     if (error) {
       return console.error(error);
     }
     
-    console.log(data);
+    console.log(data.forecast);
   })
 })
--- utils/forecast.js
+++ utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('postman-request');
 
 const forecast = (latitude, longitude, callback) => {
-  const url = `http://api.weatherstack.com/forecast?access_key=fd6621ff82539cd3e25a24d3d6243596&query=${latitude},${longitude}`;
+  const url = `http://api.weatherstack.com/forecast?units=f&access_key=fd6621ff82539cd3e25a24d3d6243596&query=${latitude},${longitude}`;
 
   request({ url, json: true }, (err, res, body) => {
     if (err) {
